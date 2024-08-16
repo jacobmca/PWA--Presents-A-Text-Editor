e7ecc21c1eb23d380e6a5c84b25c422e4e8b30d8
Jacob McAuliffe <64445624+jacobmca@users.noreply.github.com>
2024-08-16 09:52:53 -0700

    Debugging package.json, webpack.

---
+++ client/src/js/install.js
@@ -0,0 +1,25 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    butInstall.style.display = 'block';
+});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        console.log(`User resposne to the install prompt: ${outcome}`);
+        deferredPrompt = null;
+        butInstall.style,display = 'none';
+    }
+});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('J.A.T.E was installed.');
+});
