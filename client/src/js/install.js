63658ec81d924ece0d77288a3b0541f218ccaa8d
Jacob McAuliffe <64445624+jacobmca@users.noreply.github.com>
2024-08-19 21:53:08 -0700

    Changed port.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,25 +1,33 @@
+// let deferredPrompt;
+// console.log('deferredPrompt initialized', deferredPrompt);
+
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('beforeinstallprompt event fired');
     event.preventDefault();
-    deferredPrompt = event;
+    window.deferredPrompt = event;
+    console.log('deferredPrompt set:', window.deferredPrompt);
     butInstall.style.display = 'block';
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    if (deferredPrompt) {
-        deferredPrompt.prompt();
-        const { outcome } = await deferredPrompt.userChoice;
-        console.log(`User resposne to the install prompt: ${outcome}`);
-        deferredPrompt = null;
-        butInstall.style,display = 'none';
+    if (window.deferredPrompt !== undefined) {
+        console.log('Prompting install...');
+        window.deferredPrompt.prompt();
+        const { outcome } = await window.deferredPrompt.userChoice;
+        console.log(`User response to the install prompt: ${outcome}`);
+        window.deferredPrompt = null;
+        butInstall.style.display = 'none';
+    } else {
+        console.log('deferredPrompt is not defined at click time');
     }
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('J.A.T.E was installed.');
 });
